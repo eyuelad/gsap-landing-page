57201eacce36632e8d058ce8f77794752d65c86c
eyuelad <37635654+eyuelad@users.noreply.github.com>
2023-06-07 13:16:53 +0000

    Build: (3cc257a)

--- app.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>GSAP</div>
-</template>
--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
-  css: ["~/assets/style/main.css"],
-  build: {
-    transpile: ["gsap"],
-  },
-});
