3d284d4bf01da23c016cf89e69f97fe3b571cc75
Eyuel Solomon <eyuelsolomonad@gmail.com>
2023-06-07 17:46:01 +0300

    feat: Add hero section

--- app.vue
+++ app.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>GSAP</div>
+  <div>
+    <header
+      id="header"
+      class="absolute w-full z-50 bg-slate-500/5 text-slate-100"
+    >
+      <div class="2xl:container mx-auto px-6 py-6 flex justify-between">
+        <div class="">-</div>
+        <div class="text-2xl font-extrabold tracking-wider">GSAP DEMO</div>
+        <div class="">-</div>
+      </div>
+    </header>
+
+    <div class="flex flex-col gap-y-10 overflow-x-hidden">
+      <Hero />
+
+      <section class="h-[3000px]"></section>
+    </div>
+  </div>
 </template>
+
+
+<style scoped>
+#hero {
+  background-color: #1b1c1e;
+}
+</style>
---
+++ components/Hero.vue
@@ -0,0 +1,44 @@
+<template>
+  <section id="hero">
+    <div
+      class="relative 2xl:container mx-auto px-6 py-32 xl:h-screen flex items-center text-slate-100"
+    >
+      <div class="hero-image-container absolute right-0 z-10 w-1/3 rotate-90">
+        <img
+          class="aspect-square"
+          src="/images/point-yellow.png"
+          alt="hero-image"
+        />
+      </div>
+      <div class="relative z-0">
+        <div class="z-10 text-3xl font-semibold">
+          Get ready to witness a bunch of ...
+        </div>
+        <div class="z-10 text-9xl font-bold">Awesome GSAP</div>
+        <div class="relative z-0 text-9xl font-bold">Animations</div>
+        <div class="z-10 text-9xl font-bold">Enjoy!</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const { $gsap } = useNuxtApp();
+
+onMounted(() => {
+  $gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#hero",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    })
+    .to(".hero-image-container", {
+      // scale: 1.1,
+      y: "60%",
+      rotate: 35,
+    });
+});
+</script>
