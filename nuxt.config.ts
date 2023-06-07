d90736b77fbc566856121339ba68b4dc3ac85fb5
Eyuel Solomon <eyuelsolomonad@gmail.com>
2023-06-07 17:45:39 +0300

    ci: Setup for github pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/gsap-landing-page/",
+  },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/style/main.css"],
   build: {
